b55cce43d06da8868b510e926e18c217d1b304af
Nilton Amaral <nilton.a.junior@hotmail.com>
2024-04-26 23:50:28 -0300

    setup Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://85.31.62.71:3333/',
-      changeOrigin: true,
+      target: 'http://85.31.62.71:3333'
     })
   );
 };
