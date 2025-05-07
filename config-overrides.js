cbd884f0ec565cdf1861c9a2642800e0ee9c771b
Nilton Amaral <nilton.a.junior@hotmail.com>
2025-05-07 11:29:59 -0300

    refactor: remove unused fallback configurations and revert to react-scripts

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,12 @@
-module.exports = {
-    webpack: function override(config, env) {
-      config.resolve.fallback = {
-        stream: require.resolve('stream-browserify'),
-        "util": require.resolve("util/"),
-        "buffer": require.resolve("buffer/"),
-        "crypto": require.resolve("crypto-browserify"),
-        "vm": require.resolve("vm-browserify"),
-      };
-      return config;
-    },
-  };
+// module.exports = {
+//     webpack: function override(config, env) {
+//       config.resolve.fallback = {
+//         stream: require.resolve('stream-browserify'),
+//         "util": require.resolve("util/"),
+//         "buffer": require.resolve("buffer/"),
+//         "crypto": require.resolve("crypto-browserify"),
+//         "vm": require.resolve("vm-browserify"),
+//       };
+//       return config;
+//     },
+//   };
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-    resolve: {
-        fallback: {
-            "util": require.resolve("util/"),
-            "stream": require.resolve("stream-browserify"),
-            "buffer": require.resolve("buffer/"),
-            "crypto": require.resolve("crypto-browserify"),
-            "vm": require.resolve("vm-browserify"),
-        }
-    }
-  }
+// module.exports = {
+//     resolve: {
+//         fallback: {
+//             "util": require.resolve("util/"),
+//             "stream": require.resolve("stream-browserify"),
+//             "buffer": require.resolve("buffer/"),
+//             "crypto": require.resolve("crypto-browserify"),
+//             "vm": require.resolve("vm-browserify"),
+//         }
+//     }
+//   }
